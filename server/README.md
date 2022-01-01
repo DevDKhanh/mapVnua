c11cb85a389e0f4ef2a1a18e6821d4b2dce1b16f
DevDKhanh <duykhanh06.work@gmail.com>
2022-01-01 16:58:25 +0700

    server nest

--- server/README.md
+++ server/README.md
@@ -1,1 +1,3 @@
-# BaseNodeJs
+<p>Account Cloundinary</p>
+<p>username :phongnd@vmodev.com</p>
+<p>password:Phongit_1995</p>
--- server/src/routes/site.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SiteController = require('../controller/siteController');
-
-router.get('/', SiteController.index);
-router.post('/signup', SiteController.signup);
-
-module.exports = router;
