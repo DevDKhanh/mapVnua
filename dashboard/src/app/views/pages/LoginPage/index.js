eac22b8217bfcf24d163b7c0de0f2c25d6bec595
DevDKhanh <duykhanh06.work@gmail.com>
2022-01-04 11:25:10 +0700

    login page

--- dashboard/src/app/views/pages/LoginPage/index.js
+++ dashboard/src/app/views/pages/LoginPage/index.js
@@ -1,8 +1,27 @@
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import style from './LoginPage.module.scss';
+
 function LoginPage() {
 	return (
-		<>
-			<h1>Login</h1>
-		</>
+		<main className={style.page}>
+			<div className={style.mainForm}>
+				<div className={style.form}>
+					<h2 className={style.title}>Đăng nhập hệ thống</h2>
+					<form>
+						<Input className={style.input} label={'Email'} />
+						<Input
+							className={style.input}
+							label={'Password'}
+							type="password"
+						/>
+						<div className={style.groupBtn}>
+							<Button text="Đăng nhập" maxWidth primary rounded />
+						</div>
+					</form>
+				</div>
+			</div>
+		</main>
 	);
 }
 
