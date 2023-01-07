39b9b924b89495ce8eaeb8503cff73d62c75202f
Tran Duy Khanh <khanhtd@m2w.vn>
2023-01-07 23:20:05 +0700

    update

--- admin/components/site/Input/index.tsx
+++ admin/components/site/Input/index.tsx
@@ -1,45 +1,47 @@
-import InputColor from 'react-input-color';
-import style from './Input.module.scss';
+import InputColor from "react-input-color";
+import style from "./Input.module.scss";
 interface props {
-    title: string;
-    value: string;
-    type?: string;
-    name: string;
-    isColorPicker?: boolean;
-    onChange: (e: any, ...args: any[]) => void;
-    [props: string]: any;
+  title: string;
+  value: string;
+  type?: string;
+  name: string;
+  isColorPicker?: boolean;
+  onChange: (e: any, ...args: any[]) => void;
+  [props: string]: any;
 }
 
 function Input({
-    title,
-    value,
-    name,
-    type = 'text',
-    onChange,
-    isColorPicker,
-    ...props
+  title,
+  value,
+  name,
+  type = "text",
+  onChange,
+  isColorPicker,
+  ...props
 }: props) {
-    return (
-        <div className={style.groupInput}>
-            <p>{title}</p>
-            <div className={style.input}>
-                {isColorPicker && (
-                    <InputColor
-                        initialValue={value}
-                        onChange={(e) => onChange(e, name)}
-                        placement="right"
-                    />
-                )}
-                <input
-                    type={type}
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                    {...props}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.groupInput}>
+      <p>
+        {title} <span style={{ color: "red" }}>(*)</span>
+      </p>
+      <div className={style.input}>
+        {isColorPicker && (
+          <InputColor
+            initialValue={value}
+            onChange={(e) => onChange(e, name)}
+            placement="right"
+          />
+        )}
+        <input
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          {...props}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Input;
--- admin/components/site/Select/index.tsx
+++ admin/components/site/Select/index.tsx
@@ -1,84 +1,81 @@
-import clsx from 'clsx';
-import { useEffect, useRef, useState } from 'react';
-import { RiArrowDownSLine } from 'react-icons/ri';
-import style from './Select.module.scss';
+import { useEffect, useRef, useState } from "react";
+
+import { RiArrowDownSLine } from "react-icons/ri";
+import clsx from "clsx";
+import style from "./Select.module.scss";
 
 interface props {
-    title: string;
-    data: Array<any>;
-    value: string;
-    onChange?: (value: any) => void;
+  title: string;
+  data: Array<any>;
+  value: string;
+  onChange?: (value: any) => void;
 }
 
 function Select({ title, data, value, onChange }: props) {
-    const ref = useRef<any>(null);
+  const ref = useRef<any>(null);
 
-    const [isTopCurent, setIsTopCurent] = useState<any>(null);
-    const [show, setShow] = useState<boolean>(false);
+  const [isTopCurent, setIsTopCurent] = useState<any>(null);
+  const [show, setShow] = useState<boolean>(false);
 
-    useEffect(() => {
-        const handleClick = (e: any) => {
-            if (ref.current && !ref.current.contains(e.target)) {
-                setShow(false);
-            }
-        };
-        document.addEventListener('click', handleClick);
-        return () => document.removeEventListener('click', handleClick);
-    }, [ref]);
+  useEffect(() => {
+    const handleClick = (e: any) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setShow(false);
+      }
+    };
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, [ref]);
 
-    useEffect(() => {
-        if (ref.current) {
-            const winDowHeight = window.innerHeight;
-            const { y } = ref.current.getBoundingClientRect();
-            setIsTopCurent(winDowHeight / y > 2);
-        }
-    }, [ref]);
+  useEffect(() => {
+    if (ref.current) {
+      const winDowHeight = window.innerHeight;
+      const { y } = ref.current.getBoundingClientRect();
+      setIsTopCurent(winDowHeight / y > 2);
+    }
+  }, [ref]);
 
-    return (
-        <div className={style.groupInput}>
-            <p>{title}</p>
-            <div
-                className={style.select}
-                ref={ref}
-                onClick={() => setShow(!show)}
-            >
-                <div>
-                    <p>
-                        {value || (
-                            <span className={style.placeholder}>
-                                Chọn {title.toLowerCase()}
-                            </span>
-                        )}
-                    </p>
-                </div>
-                <span className={style.icon}>
-                    <RiArrowDownSLine />
-                </span>
-                {show && (
-                    <div
-                        className={clsx(style.list, {
-                            [style.listTop]: !isTopCurent,
-                        })}
-                    >
-                        {data.map((item, i) => (
-                            <div
-                                key={i}
-                                className={style.item}
-                                onClick={() => !!onChange && onChange(item)}
-                            >
-                                {item.txt}
-                            </div>
-                        ))}
-                        {data.length <= 0 && (
-                            <div className={style.item}>
-                                Không có dữ liệu phù hợp
-                            </div>
-                        )}
-                    </div>
-                )}
-            </div>
+  return (
+    <div className={style.groupInput}>
+      <p>
+        {title} <span style={{ color: "red" }}>(*)</span>
+      </p>
+      <div className={style.select} ref={ref} onClick={() => setShow(!show)}>
+        <div>
+          <p>
+            {value || (
+              <span className={style.placeholder}>
+                Chọn {title.toLowerCase()}
+              </span>
+            )}
+          </p>
         </div>
-    );
+        <span className={style.icon}>
+          <RiArrowDownSLine />
+        </span>
+        {show && (
+          <div
+            className={clsx(style.list, {
+              [style.listTop]: !isTopCurent,
+            })}
+          >
+            {data.map((item, i) => (
+              <div
+                key={i}
+                className={style.item}
+                onClick={() => !!onChange && onChange(item)}
+              >
+                {item.txt}
+              </div>
+            ))}
+            {data.length <= 0 && (
+              <div className={style.item}>Không có dữ liệu phù hợp</div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Select;
